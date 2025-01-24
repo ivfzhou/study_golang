3e11a17c5e4d53212ddff885f429e7c0c3b40d28
ivfzhou <ivfzhou@126.com>
2025-01-24 23:00:11 +0800

    添加 garbageNoFree 函数

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
 	"unsafe"
 )
 
 func main() {
-	to := 1
+	to := 3
 	switch to {
 	case 1:
 		x := byte(1)
@@ -18,6 +20,55 @@ func main() {
 			y float32
 		}{x: int32(x), y: y}
 		bitPrint(s)
+	case 2:
+		garbage()
+	case 3:
+		garbageNoFree()
+	}
+}
+
+func garbage() {
+	type S struct {
+		Field *int
+	}
+
+	func() {
+		f := new(int)
+		s := new(S)
+		s.Field = f
+		runtime.SetFinalizer(s, func(s *S) {
+			fmt.Println("s is freed")
+		})
+		runtime.SetFinalizer(f, func(s *int) {
+			fmt.Println("f is freed")
+		})
+	}()
+	for i := 0; i < 10; i++ {
+		runtime.GC()
+		time.Sleep(time.Second)
+	}
+}
+
+func garbageNoFree() {
+	type S struct {
+		Field *S
+	}
+
+	func() {
+		f := new(S)
+		s := new(S)
+		s.Field = f
+		f.Field = s
+		runtime.SetFinalizer(s, func(s *S) {
+			fmt.Println("s is freed")
+		})
+		runtime.SetFinalizer(f, func(s *S) {
+			fmt.Println("f is freed")
+		})
+	}()
+	for i := 0; i < 10; i++ {
+		runtime.GC()
+		time.Sleep(time.Second)
 	}
 }
 
