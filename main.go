921ec4dfcb381579fcf037f4c4d34a5b7adb6780
ivfzhou <ivfzhou@126.com>
2025-01-24 22:17:17 +0800

    添加 bitPrint 函数

--- main.go
+++ main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	to := 1
+	switch to {
+	case 1:
+		x := byte(1)
+		bitPrint(x)
+		y := float32(7.3125)
+		bitPrint(y)
+		s := struct {
+			x int32
+			y float32
+		}{x: int32(x), y: y}
+		bitPrint(s)
+	}
+}
+
+func bitPrint[T any](t T) {
+	size := int64(unsafe.Sizeof(t))
+	ptr := unsafe.Pointer(&t)
+	var b byte
+	for i := size - 1; i > 0; i-- {
+		b = *(*byte)(unsafe.Add(ptr, i))
+		for j := 7; j >= 0; j-- {
+			fmt.Printf("%d", b>>j&0b1)
+		}
+		fmt.Print("_")
+	}
+	b = *(*byte)(ptr)
+	for j := 7; j >= 0; j-- {
+		fmt.Printf("%d", b>>j&0b1)
+	}
+	fmt.Println()
+}
